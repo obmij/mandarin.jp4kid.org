bc6ea23e18770c69bdfd41110060202d1365fa2b
obmij <82932766+obmij@users.noreply.github.com>
2025-06-09 10:01:34 +0800

    Add files via upload

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "救助儿童必要从预防暴力开始",
+        "excerpt":"","categories": ["learn"],
+        "tags": [],
+        "url": "http://localhost:4000/learn/save-children.html"
+      },{
+        "title": "了解关键词汇",
+        "excerpt":"#儿童权利公约；CRC人人有权享有公约所载的一切权利和自由，不分种族、肤色、性别、语言、宗教、政治或其他见解、国籍或社会出身、财产、残疾、出生或其他身分等任何区别。儿童有权享有特殊照顾和援助。儿童是指未满十八岁的任何人（除非根据适用于儿童的法律，成年年龄提前）。儿童的最大利益应是首要考虑因素。确保儿童获得其福祉所需的保护和照顾。 #毒性压力 ","categories": ["learn"],
+        "tags": [],
+        "url": "http://localhost:4000/learn/Understand-the-terms.html"
+      }]
